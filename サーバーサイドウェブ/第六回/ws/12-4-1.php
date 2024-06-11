e2a461b555141b6b6e0edf6dfcb4e8d6370cd370
HirokiAndo22 <hirokia2280@gmail.com>
2024-06-11 12:57:33 +0900

    追加

---
+++ サーバーサイドウェブ/第六回/ws/12-4-1.php
@@ -0,0 +1,24 @@
+<?php
+    $data = $_GET["data"];
+    $data = mb_convert_kana($data,"KVa");
+    $data *= 1;
+    if($data < 50){
+        $str = "低い";
+    }elseif( $data > 50){
+        $str = "高い";
+    }else{
+        $str = "正解";
+    }
+?>
+
+<!DOCTYPE>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>6-2</title>
+    </head>
+    <body>
+        <p>あなたは<?= $str;?></p>
+        <p><a href="12-4.php">入力へ戻る</a></p>
+    </body>
+</html>
